ba37e74cb45be043c9f8275eff36bb15104b65c0
aleksey-uvarov <34627976+aleksey-uvarov@users.noreply.github.com>
2019-05-21 17:30:32 +0300

    Presumably fixed one bug but now it is even worse
    Previously fixed bug: Downsweep would not work well for 4 or more blocks
    
    Currently discovered/created bug: the output of the program overall is not deterministic

--- scan.cu
+++ scan.cu
@@ -50,7 +50,7 @@ __global__ void blelloch_downsweep(float * d_in, int n_points)
     }
     __syncthreads();
     
-    while(n_current >= 1)
+    while(n_current >= 2)
     {
         if ((id + 1) % n_current == 0)
         {
@@ -58,18 +58,44 @@ __global__ void blelloch_downsweep(float * d_in, int n_points)
             d_in[id] += d_in[id - n_current/2];
             d_in[id - n_current/2] = tmp;
         }
-        __syncthreads();
         n_current = n_current / 2;
+        __syncthreads();
     }
 }
+
+__global__ void blelloch_downsweep_step(float * d_in, int n_points, int stride_step)
+{
+    
+    int id = threadIdx.x + blockIdx.x * blockDim.x;       
+    float tmp;
+
+    
+    if (id == n_points - 1 && stride_step == n_points)
+    {
+        d_in[id] = 0;
+    }
+    __syncthreads();
+    
+ 
+    if ((id + 1) % stride_step == 0)
+    {
+        tmp = d_in[id];
+        d_in[id] += d_in[id - stride_step/2];
+        d_in[id - stride_step/2] = tmp;
+    }
+    __syncthreads();
+}
+
 
 int main(int argc, char* argv[])
 {
     float minus_infty = -8;
     float x_max = 0;
-    int n_blocks = 16;
-    long int n_points = 1024 * n_blocks;
-
+    int n_blocks = 64;
+    int n_points_per_block = 1024;
+    int n_points = n_points_per_block * n_blocks;
+    int stride_step = n_points;
+    
     float dx;
     float *devFunVals;
 //     float *devScan;
@@ -93,11 +119,12 @@ int main(int argc, char* argv[])
         return 0;
     }
     dx = (x_max - minus_infty) / (float) n_points;
-    printf("dx: %e\n ", dx);
-    
+//     printf("dx: %e\n", dx);
+//     printf("n_points: %d\n", n_points);
+//     
     if (n_points < 0 || ((n_points & (n_points - 1)) != 0))
     {
-        printf("n_points is not a power of two");
+        printf("n_points is not a power of two\n");
         return 1;
     }
     
@@ -109,28 +136,37 @@ int main(int argc, char* argv[])
     
 //     CUDA_CALL(cudaMalloc((void **)&devScan, n_points*sizeof(float)));
     
-    prepare_function<<<n_blocks, n_points/n_blocks>>>(devFunVals, n_points, minus_infty, x_max);
+    prepare_function<<<n_blocks, n_points_per_block>>>(devFunVals, n_points, minus_infty, x_max);
+    
+    blelloch_reduce<<<n_blocks, n_points_per_block>>>(devFunVals, n_points);
     
     CUDA_CALL(cudaMemcpy(hostFunVals, devFunVals, n_points*sizeof(float), cudaMemcpyDeviceToHost));
     
-    blelloch_reduce<<<n_blocks, (int) n_points/n_blocks>>>(devFunVals, n_points);
-    blelloch_downsweep<<<n_blocks, (int) n_points/n_blocks>>>(devFunVals, n_points);
+
+//     blelloch_downsweep<<<n_blocks, n_points_per_block>>>(devFunVals, n_points);
+    
+    while(stride_step >= 2)
+    {
+        blelloch_downsweep_step<<<n_blocks, n_points_per_block>>>(devFunVals, n_points, stride_step);
+        stride_step = stride_step / 2; 
+    }
 
 
     CUDA_CALL(cudaMemcpy(hostFunVals2, devFunVals, n_points*sizeof(float), cudaMemcpyDeviceToHost));
 
 //     for(int i=0; i<n_points; i++)
 //     {
-//         printf("%1.4f ", hostFunVals[i]);
+//         printf("%1.4f \n", hostFunVals[i]);
 //     }
-//     printf("\n");
+//     printf("=========\n");
 //     
 //     for(int i=0; i<n_points; i++)
 //     {
-//         printf("%1.4f ", hostFunVals2[i]);
+//         printf("%1.4f \n", hostFunVals2[i]);
 //     }
 //     printf("\n");
-    printf("%1.5f\n", hostFunVals2[n_points - 1] * dx);
+//     printf("Func value: %1.5f\n", hostFunVals[n_points - 1]);
+    printf("Integral value: %1.5f\n", hostFunVals2[n_points - 1] * dx);
     
     
     
@@ -143,7 +179,7 @@ int main(int argc, char* argv[])
     
     
     
-//     free(hostFunVals);
+    free(hostFunVals);
     free(hostFunVals2);
 
 //     CUDA_CALL(cudaFree(devFunVals));
